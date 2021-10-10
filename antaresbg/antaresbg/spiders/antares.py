360277336d3a709ff89904946c1222e8c826467c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-10-10 13:41:52 +0300

    Data scraping from https://antares-bg.net/

---
+++ antaresbg/antaresbg/spiders/antares.py
@@ -0,0 +1,39 @@
+import scrapy
+# from scrapy_splash import SplashRequest
+import logging
+
+
+class AntaresSpider(scrapy.Spider):
+    name = 'antares'
+    allowed_domains = ['www.antares-bg.net']
+    start_urls = ['https://antares-bg.net/ofis-stolove-ceni']
+
+    # url = "https://antares-bg.net/ofis-stolove-ceni"
+    #
+    # script = '''
+    # function main(splash, args)
+    #     splash:set_user_agent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36')
+    #     splash.private_mode_enabled = false
+    #     assert(splash:go(args.url))
+    #     assert(splash:wait(1))
+    #     splash:set_viewport_full()
+    #     return {splash:html()}
+    # end
+    # '''
+    # def start_requests(self):
+    #     yield SplashRequest(url=self.url,
+    #                         callback=self.parse,
+    #                         endpoint='execute',
+    #                         args={'lua_source': self.script})
+
+    def parse(self, response, **kwargs):
+        domain = "https://antares-bg.net"
+        elements = response.xpath('//li[@class="col-md-4 product_list"]')
+        for e in elements:
+            name = e.xpath('.//a/@title').get()
+            link = e.xpath('.//a/@href').get()
+
+            yield {
+                "name": name,
+                "link": f"{domain}{link}"
+            }
